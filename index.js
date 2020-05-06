7b7a7d8d1ab93de2841e1be8b6c7e6d1e8f28ab3
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-06 00:52:58 -0500

    add media query to finally make the layout work

--- index.js
+++ index.js
@@ -10,4 +10,25 @@ const toggleMenu = () => {
 hamburger.addEventListener(`click`, toggleMenu);
 closeButton.addEventListener(`click`, toggleMenu);
 
+const scrollToAccept = () => {
+  const button = document.querySelector(`.accept`);
 
+  if (!menu) {
+    return; // quit if there isn't the terms item on the page
+  }
+
+  const obCallback = payload => {
+    if (payload[0].intersectionRatio === 1) {
+      button.disabled = false;
+      ob.unobserve(terms.lastElementChild);
+    }
+  };
+  const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+  });
+
+  ob.observe(terms.lastElementChild);
+};
+
+scrollToAccept();
