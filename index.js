efc2c6017637709587c4aa9fa5f987adc16791c3
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-06 00:38:20 -0500

    get disable scroll working

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const hamburger = document.querySelector(`.hamburger`);
 const menu = document.querySelector(`.menu`);
 const closeButton = document.querySelector(`.close`);
 
-const toggleMenu = () => menu.classList.toggle(`open`);
+const toggleMenu = () => {
+  menu.classList.toggle(`open`);
+  document.querySelector(`body`).classList.toggle(`disable-scroll`);
+};
 
 hamburger.addEventListener(`click`, toggleMenu);
 closeButton.addEventListener(`click`, toggleMenu);
+
+
