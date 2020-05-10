1673b972215bafbab06b062841e464c58e706c99
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-09 19:31:12 -0500

    add dynamically generated SwipeOver

--- menu.js
+++ menu.js
@@ -1,13 +1,21 @@
-const siteNav-hamburger = document.querySelector(`.siteNav-hamburger`);
+const siteNavHamburger = document.querySelector(`.siteNav-hamburger`);
 const swipeOver = document.querySelector(`.swipeOver`);
 const closeButton = document.querySelector(`.close`);
 
 const toggleswipeOver = () => {
-  if (siteNav-hamburger && swipeOver && closeButton) {
+  if (siteNavHamburger && swipeOver && closeButton) {
     swipeOver.classList.toggle(`swipeOver-isOpen`);
   }
   document.querySelector(`body`).classList.toggle(`disable-scroll`);
 };
 
-siteNav-hamburger.addEventListener(`click`, toggleswipeOver);
+siteNavHamburger.addEventListener(`click`, toggleswipeOver);
 closeButton.addEventListener(`click`, toggleswipeOver);
+
+// Dym=namically generate the list of links based on the content
+const menuItems = document.querySelectorAll(`.menuList-link`);
+const swipeOverLinkList = document.querySelector(`.swipeOver-linkList`);
+menuItems.forEach(item => {
+  const li = `<li class="swipeOver-link">${item.outerHTML}</li>`;
+  swipeOverLinkList.insertAdjacentHTML(`beforeend`, li);
+});
