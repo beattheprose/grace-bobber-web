cc1d60e177c8710a93d8585f80cc8f004d58689f
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-17 21:08:25 -0500

    got it working!

--- scripts/videotrans.ts
+++ scripts/videotrans.ts
@@ -1,15 +1,19 @@
-const scrollFunc =
-  window.requestAnimationFrame ||
-  function(callback) {
-    window.setTimeout(callback, 1000 / 60);
-  };
-
-const nav = document.querySelector(`.nav`) as HTMLElement;
 const video = document.querySelector(`.video`) as HTMLElement;
+const nav = document.querySelector(`.siteNav`) as HTMLElement;
+const link = document.querySelector(`.menuList-link`) as HTMLElement;
+const content = document.querySelector(`.content`) as HTMLElement;
 
 const bounding = video.getBoundingClientRect();
-const percentageIn = ((bounding.height + bounding.y) / bounding.height) * 100;
+const bounding2 = content.getBoundingClientRect();
 
-console.log(bounding);
+console.log(bounding2);
 
-console.log(percentageIn);
+const scrollCallbackHandler = () => {
+  let alpha =
+    1 -
+    (bounding.height - document.documentElement.scrollTop) / bounding.height;
+  nav.style.backgroundColor = `rgba(255,255,255,${alpha})`;
+  link.style.color = `rgba(255,255,255,${alpha})`;
+};
+
+window.addEventListener(`scroll`, scrollCallbackHandler);
