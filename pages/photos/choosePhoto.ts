23926f0061994524c43dec0861b5f21282f8b675
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-21 10:52:51 -0500

    add download functionality to big image

--- pages/photos/choosePhoto.ts
+++ pages/photos/choosePhoto.ts
@@ -1,10 +1,13 @@
 const imageList = document.querySelectorAll(`.gallery-image`);
 const bigImage = document.querySelector(`.big-image`) as HTMLImageElement;
+const downloadWrapper = document.querySelector(
+  `.download-wrapper`
+) as HTMLLinkElement;
 
 const handleClick = (e) => {
   bigImage.src = e.currentTarget.src;
+  downloadWrapper.href = e.currentTarget.src;
   e.currentTarget.classList.add(`selected`);
-  console.log(e.currentTarget, imageList);
   imageList.forEach((image) => {
     image !== e.currentTarget ? image.classList.remove(`selected`) : null;
   });
