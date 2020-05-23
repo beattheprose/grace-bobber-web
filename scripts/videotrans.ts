4c38b146ecae0056b272759b91e44ab1de85cae8
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-22 21:19:09 -0500

    committing ahead of transfer to homepage

--- scripts/videotrans.ts
+++ scripts/videotrans.ts
@@ -1,21 +1,21 @@
-// const nav = document.querySelector(`.siteNav`) as HTMLElement;
+const nav = document.querySelector(`.siteNav`) as HTMLElement;
 
-// let videoWrapperHeight = document
-//   .querySelector(`.video`)
-//   .getBoundingClientRect().height;
-// const navBounding = nav.getBoundingClientRect();
+let videoWrapperHeight = document
+  .querySelector(`.video`)
+  .getBoundingClientRect().height;
+const navBounding = nav.getBoundingClientRect();
 
-// function scrollCallbackHandler() {
-//   let ratioVisible =
-//     1 -
-//     (videoWrapperHeight - document.documentElement.scrollTop) /
-//       videoWrapperHeight;
-//   const heroWrapper = document.querySelector(`.heroWrapper`) as HTMLElement;
-//   if (ratioVisible > 0) {
-//     heroWrapper.classList.add(`heroWrapper-disabled`);
-//   } else {
-//     heroWrapper.classList.remove(`heroWrapper-disabled`);
-//   }
-// }
+function scrollCallbackHandler() {
+  let ratioVisible =
+    1 -
+    (videoWrapperHeight - document.documentElement.scrollTop) /
+      videoWrapperHeight;
+  const heroWrapper = document.querySelector(`.heroWrapper`) as HTMLElement;
+  if (ratioVisible > 0) {
+    heroWrapper.classList.add(`heroWrapper-disabled`);
+  } else {
+    heroWrapper.classList.remove(`heroWrapper-disabled`);
+  }
+}
 
-// window.addEventListener(`scroll`, scrollCallbackHandler);
+window.addEventListener(`scroll`, scrollCallbackHandler);
