864a4f06e47b14d4243fac1430ed204d1e86077b
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-23 18:25:59 -0500

    get the submenu working again

--- scripts/menu.ts
+++ scripts/menu.ts
@@ -1,4 +1,4 @@
-//sticky, but not annoying nav
+// sticky, but not annoying nav
 import Headroom from "headroom.js";
 const siteNav = document.querySelector(`nav`) as HTMLElement;
 const videoWrapper = document.querySelector(`.videoWrapper`) as HTMLElement;
@@ -9,13 +9,9 @@ videoWrapper
 var headroom = new Headroom(siteNav, options);
 headroom.init();
 
-const siteNavHamburger = document.querySelector(
-  `.siteNav-hamburger`
-) as HTMLElement;
+// activate submenus on swipeOver
 const swipeOver = document.querySelector(`.swipeOver`) as HTMLElement;
-const closeButton = document.querySelector(`.close`) as HTMLElement;
-
-const swipeOverDropDown = swipeOver.querySelectorAll(`.menuList-dropdown`);
+const swipeOverDropDown = swipeOver.querySelectorAll(`.swipeOver-dropdown`);
 
 const handleSwipeClick = (e) => {
   e.preventDefault();
@@ -29,6 +25,12 @@ swipeOverDropDown.forEach((dropDown: HTMLElement) => {
   dropDown.previousElementSibling.addEventListener(`click`, handleSwipeClick);
 });
 
+// make the hamburger menu open the swipeOver
+const siteNavHamburger = document.querySelector(
+  `.siteNav-hamburger`
+) as HTMLElement;
+const closeButton = document.querySelector(`.close`) as HTMLElement;
+
 const swipeOverToggle = () => {
   if (siteNavHamburger && swipeOver && closeButton) {
     swipeOver.classList.toggle(`swipeOver-isOpen`);
@@ -38,14 +40,3 @@ const swipeOverToggle = () => {
 
 siteNavHamburger.addEventListener(`click`, swipeOverToggle);
 closeButton.addEventListener(`click`, swipeOverToggle);
-
-// Dynamically generate the list of links based on the content
-// const menuItems = document.querySelectorAll(`.menuList-link`);
-// const swipeOverLinkList = document.querySelector(`.swipeOver-linkList`);
-
-// menuItems.forEach((item: { outerHTML: string }) => {
-//   const li: string = `<li class="swipeOver-link">${item.outerHTML}</li>`;
-//   swipeOverLinkList.insertAdjacentHTML(`beforeend`, li);
-// });
-
-// Dynamically Generate the second swipeover
