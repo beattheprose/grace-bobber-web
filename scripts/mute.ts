a190ca3fe8843d60e0eb57ce090d127b8ee600e0
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-23 21:53:52 -0500

    get everything fully working

--- scripts/mute.ts
+++
@@ -1,8 +0,0 @@
-const muteButton = document.querySelector(`.mutebutton`) as HTMLElement;
-const video = document.querySelector(`.video`) as any;
-
-const handleClick = (e) => {
-  video.muted ? (video.muted = false) : (video.muted = true);
-};
-
-muteButton.addEventListener(`click`, handleClick);
