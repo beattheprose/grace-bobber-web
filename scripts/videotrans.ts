1984f6261e16935ee1e32f21b44c47b3ea20f440
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-23 22:14:52 -0500

    change the button animation to turn off after click

--- scripts/videotrans.ts
+++ scripts/videotrans.ts
@@ -29,6 +29,7 @@ const video = document.querySelector(`.video`) as any;
 
 const handleClick = (e) => {
   video.muted ? (video.muted = false) : (video.muted = true);
+  muteButton.style.removeProperty(`animation`);
 };
 
 muteButton.addEventListener(`click`, handleClick);
