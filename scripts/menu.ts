22b05a6c234908b4cdda7fd442a21093e6ceadae
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-27 17:38:12 -0500

    prepos sucks

--- scripts/menu.ts
+++ scripts/menu.ts
@@ -10,20 +10,7 @@ var headroom = new Headroom(siteNav, options);
 headroom.init();
 
 // activate submenus on swipeOver
-const swipeOver = document.querySelector(`.swipeOver`) as HTMLElement;
-const swipeOverDropDown = swipeOver.querySelectorAll(`.swipeOver-dropdown`);
-
-const handleSwipeClick = (e) => {
-  e.preventDefault();
-  swipeOverDropDown.forEach((element) =>
-    element.classList.remove(`swipeOver-dropdown-isOpen`)
-  );
-  e.currentTarget.nextElementSibling.classList.add(`swipeOver-dropdown-isOpen`);
-};
-
-swipeOverDropDown.forEach((dropDown: HTMLElement) => {
-  dropDown.previousElementSibling.addEventListener(`click`, handleSwipeClick);
-});
+const swipeOverDropDown = document.querySelectorAll(`.swipeOver-dropdown`);
 
 // make the hamburger menu open the swipeOver
 const siteNavHamburger = document.querySelector(
@@ -42,12 +29,44 @@ const swipeOverToggle = () => {
   }
 };
 
-document.addEventListener(`click`, (e) =>
+const swipeOverList = document.querySelector(`.swipeOver-linkList`);
+
+document.addEventListener(`click`, (e: any) => {
+  // listen for clicks on all the menu buttons
   e.target === siteNavHamburger ||
   e.target === siteNavHamburgerPath ||
   e.target === closeButton ||
   e.target === bioLink ||
   e.target === mobileMenuButton
     ? swipeOverToggle()
-    : null
-);
+    : null;
+
+  console.log(e.target);
+});
+
+const handleSiteNavHamburger = (e) => {
+  const isHamburger: boolean =
+    e.target.matches(`.siteNav-hamburger`) &&
+    e.target.parentElement.matches(`.siteNav-hamburger`);
+  if (!isHamburger) return;
+  console.log(`fuck you`);
+};
+
+const handleSwipeOverDropdown = (e) => {
+  // make sure it has a nextElementSibling
+  if (e.target.nextElementSibling) {
+    // delegate event
+    if (!e.target.nextElementSibling.matches(`.swipeOver-dropdown`)) return;
+    //execute code
+    e.preventDefault();
+    swipeOverDropDown.forEach((element) =>
+      element.classList.remove(`swipeOver-dropdown-isOpen`)
+    );
+    e.target.nextElementSibling.classList.add(`swipeOver-dropdown-isOpen`);
+  }
+};
+
+document.addEventListener(`click`, (e) => {
+  handleSiteNavHamburger(e);
+  handleSwipeOverDropdown(e);
+});
